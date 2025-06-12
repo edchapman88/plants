bc5d4c8844f5e0ff652492c7e2b68c5772d576b2
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-06-12 14:45:29 +0100

    diffusion with local d

--- lib/diffuse.ml
+++ lib/diffuse.ml
@@ -1,25 +1,22 @@
-type t = (float, float) Mesh.t
+type node = {
+  quant : float;
+  d : float;
+}
 
-(* let linear m c x = (m *. float_of_int x) +. c *)
-(* let w_sat = 128 *)
-(* let perm = 1.0/.32.0 *)
-(* let k_unsat = linear 0.5 0.0 *)
-(* let k_sat = linear 1.0 0.0 *)
+type t = (node, float) Mesh.t
+(** [(node, edge) Mesh.t] *)
 
-(* let pressure_of_w w = if w < w_sat then k_unsat w else k_sat w *)
-(* let pressure_grad wa wb = pressure_of_w wb -. pressure_of_w wa *)
-(* let flux ~perm wa wb = perm *. (pressure_grad wa wb) *)
-
-let grad a b = b -. a
+let grad a b = b.quant -. a.quant
 let of_grid grid = Mesh.of_nodes ~edge_fn:grad grid
 
-let diffuse ~d mesh =
+let diffuse mesh =
   let open Mesh in
   mapi ~edge_fn:grad
-    (fun xy node ->
+    (fun xy { quant; d } ->
       let edge_of = edge_at mesh xy in
       let delta =
         edge_of Left -. edge_of Right +. (edge_of Up -. edge_of Down)
       in
-      node +. (-1.0 *. d *. delta))
+      let quant' = quant +. (-1.0 *. d *. delta) in
+      { quant = quant'; d })
     mesh
--- test/test_diffuse.ml
+++ test/test_diffuse.ml
@@ -1,18 +1,55 @@
 open Plants.Diffuse
 open Plants.Mesh
 
-let arrange_float_grid () = Grid.of_arrays [| [| 0.0; 1.0 |]; [| 2.0; 3.0 |] |]
+let arrange_node_grid () =
+  Grid.mapi
+    (fun _ q -> { quant = q; d = 0.1 })
+    (Grid.of_arrays [| [| 0.0; 1.0 |]; [| 2.0; 3.0 |] |])
 
 let print_mesh m =
-  let str_el = Printf.sprintf "%.3f" in
+  let str_el node = Printf.sprintf "%.3f" node.quant in
   print_endline (string_of_mesh ~str_el m)
 
 let%expect_test "diffuse" =
-  let m = of_grid (arrange_float_grid ()) in
+  let m = of_grid (arrange_node_grid ()) in
   let rec loop n mesh =
     if n < 0 then ()
     else (
       print_mesh mesh;
-      loop (n - 1) (diffuse ~d:0.1 mesh))
+      loop (n - 1) (diffuse mesh))
   in
-  loop 10 m
+  loop 10 m;
+  [%expect {|
+    0.000 1.000
+    2.000 3.000
+
+    0.600 1.200
+    1.800 2.400
+
+    0.960 1.320
+    1.680 2.040
+
+    1.176 1.392
+    1.608 1.824
+
+    1.306 1.435
+    1.565 1.694
+
+    1.383 1.461
+    1.539 1.617
+
+    1.430 1.477
+    1.523 1.570
+
+    1.458 1.486
+    1.514 1.542
+
+    1.475 1.492
+    1.508 1.525
+
+    1.485 1.495
+    1.505 1.515
+
+    1.491 1.497
+    1.503 1.509
+    |}]
